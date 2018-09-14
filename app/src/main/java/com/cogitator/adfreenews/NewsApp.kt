292632fb98b4225996749566dcac9e91d2028039
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-09-14 15:16:49 +0800

    main

--- app/src/main/java/com/cogitator/adfreenews/NewsApp.kt
+++ app/src/main/java/com/cogitator/adfreenews/NewsApp.kt
@@ -1,6 +1,9 @@
 package com.cogitator.adfreenews
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 1/9/18 (MM/DD/YYYY)
